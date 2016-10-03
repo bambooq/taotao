99d0d716a628ca3369acbbd1f3eb22b6144de1dd
zhengqi <bamboo625>
2016-10-03 20:44:36 +0800

    框架整合及测试

---
+++ taotao-manager-service/src/main/java/com/taotao/service/ItemService.java
@@ -0,0 +1,10 @@
+package com.taotao.service;
+
+import com.taotao.pojo.TbItem;
+
+/**
+ * Created by zhengqi on 2016/10/3.
+ */
+public interface ItemService {
+    TbItem getItemById(long itemId);
+}
